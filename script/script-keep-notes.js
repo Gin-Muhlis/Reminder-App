8e5bf8f65b3c2f23563eec86bdb78b1d91c6581d
Gin Gin Nurilham M <ginnncore717@gmail.com>
2022-12-10 12:08:11 +0700

    mengganti kode script javascript dengan yang lebih efektif

--- script/script-keep-notes.js
+++ script/script-keep-notes.js
@@ -1,139 +1,148 @@
-// !show pop up for add notes
-document.querySelector('#addNote').onclick = function () {
-  document.querySelector('.pop-up').classList.add('show');
-}
+// !deklarasi variabel
+const addButtonPopupAdd = document.querySelector("#addNote");
+const addTitle = document.querySelector('#titleNotes');
+const addText = document.querySelector('#contentNotes');
+const addPopup = document.querySelector(".pop-up");
+const addButtonNote = document.querySelector('#tambah')
+const closeButtonPopupAdd = document.querySelector('#batal');
+const editPopup = document.querySelector('.pop-up-edit');
+const editTitle = document.querySelector('#title-edit');
+const editText = document.querySelector('#content-edit');
+const editButtonNote = document.querySelector('#edit')
+const alertTextAdd = document.querySelector('.form .alert');
+const alertTextEdit = document.querySelector('.form-edit .alert');
 
-// !hide pop up for add notes
-document.querySelector('#batal').onclick = function () {
-  document.querySelector('.pop-up').classList.remove('show');
+addButtonPopupAdd.addEventListener('click', () => {
+  addPopup.classList.add('show'); // !menampilkan popup untuk menambahkan note
+})
 
-  document.querySelector('#titleNotes').value = "";
-  document.querySelector('#contentNotes').value = "";
-  document.querySelector('.alert').style.opacity = 0;
-}
+closeButtonPopupAdd.addEventListener('click', () => {
+  addPopup.classList.remove('show'); // !menyembunyikan popup untuk menambahkan note
+})
 
-// !make a dinamis notes
-document.querySelector('#tambah').onclick = function () {
-  addNote();
-}
 
-// !add note function
-function addNote() {
-  let judulNotes = document.querySelector('#titleNotes');
-  let contentNotes = document.querySelector('#contentNotes');
+addButtonNote.addEventListener('click', () => {
+  let notes = localStorage.getItem('NOTES'); // !mengambil data dari local storage
 
-  if (judulNotes.value.length == 0 || contentNotes.value.length == 0) {
-    document.querySelector('.alert').style.opacity = 1;
+  if (addTitle.value.length === 0 || addText.value.length === "") { // !mengecek jika user tidak mengisi judul atau isi note
+    alertTextAdd.style.opacity = 1; // !tampilkan pesan peringatan untuk mengisi judul dan isi note
 
     setTimeout(() => {
-      document.querySelector('.alert').style.opacity = 0;
-    }, 5000)
+      alertTextAdd.style.opacity = 0; // !hilangkan kembali pesan peringatan setelah 5 detik
+    }, 5000);
   } else {
-    document.querySelector('.notes .row').innerHTML +=
-      ` <div class="col" style="background: #${randomColor()};">
-    <div class="content">
-      <div class="notes-title">${judulNotes.value}</div>
-      <div class="notes-content">${contentNotes.value}</div>
-    </div>
-    <div class="bottom-note">
-      <div class="date">
-        ${dateNow()}
+
+    if (notes === null) { // !mengecek apakah ada data atau tidak di local storage
+      noteObj = []; // !jika tidak ada kembalikan array kosong di noteObj
+    } else {
+      noteObj = JSON.parse(notes); // !Jika ada kembalikan datanya dalanya dengan mengubah datanya dari string menjadi object
+    }
+
+    let myObj = { // !membuat object untuk menyimpan data judul dan isi note
+      title: addTitle.value,
+      text: addText.value,
+      bgColor: randomColor()
+    }
+
+    noteObj.push(myObj); // !menyimpan data setiap note didalam array
+
+    localStorage.setItem('NOTES', JSON.stringify(noteObj)); // !mengirim data setiap note dalam bentuk string
+
+    addTitle.value = ""; // !mereset input field dengan string kosong
+    addText.value = ""; // !mereset input field dengan string kosong
+
+    addPopup.classList.remove('show'); // !menyembunyikan pop up menambahkan note
+
+    renderNotes(); // !memanggil functin untuk menampilkan note
+
+  }
+
+})
+
+// !membuat function untuk menampilkan notes
+function renderNotes() {
+  let notes = localStorage.getItem('NOTES'); // !mengambil data dari local storage 
+
+  if (notes === null) { // !mengecek apakah ada data atau tidak di local storage
+    noteObj = []; // !jika tidak ada kembalikan array kosong di noteObj
+  } else {
+    noteObj = JSON.parse(notes); // !Jika ada kembalikan datanya dalanya dengan mengubah datanya dari string menjadi object
+  }
+
+  let templeteHTML = "";
+
+  noteObj.forEach((element, index) => {
+    templeteHTML += `<div class="col" style="background: ${element.bgColor};">
+      <div class="content">
+        <div class="notes-title" id="title${index}">${element.title}</div>
+        <div class="notes-content" id="content${index}">${element.text}</div>
       </div>
-      <div class="button">
-        <div class="starField">
-          <i class="fa-regular fa-star star"></i>
+      <div class="bottom-note">
+        <div class="date">
+          ${dateNow()}
         </div>
-        <span class="edit">
-          <i class="fa-solid fa-pen"></i>
-        </span>
+        <div class="button">
+          <div class="starField">
+            <i class="fa-regular fa-star star"></i>
+          </div>
+          <span class="edit" id="${index}" onclick="editNote(this.id)">
+            <i class="fa-solid fa-pen"></i>
+          </span>
+        </div>
       </div>
-    </div>
-  </div>`
+    </div>`
+  });
 
-    // !hidden popup add notes
-    document.querySelector('.pop-up').classList.remove('show');
-    document.querySelector('.alert').style.opacity = 0;
-
-    // !Reset field input
-    judulNotes.value = "";
-    contentNotes.value = "";
-
-    // !starred notes
-    starredNotes();
-
-    // !edit notes
-    editNote();
-
+  let rowElement = document.querySelector('.row');
+  if (noteObj.length != 0) {
+    rowElement.innerHTML = templeteHTML;
+  } else {
+    rowElement.innerHTML = "";
   }
 }
 
-// !edit note function 
-function editNote() {
-  let editButton = document.querySelectorAll('.edit');
-    let titleNotesEdit = document.querySelectorAll('.notes-title');
-    let contentNotesEdit = document.querySelectorAll('.notes-content');
+// !membuat function untuk mengedit note
+function editNote(index) {
+  let notes = localStorage.getItem('NOTES'); // !mengambil data dari local storage 
 
-    for (let i = 0; i < editButton.length; i++) {
-      editButton[i].onclick = function() {
-        let titleEdit = document.querySelector('#title-edit');
-        let contentEdit = document.querySelector('#content-edit');
+  addPopup.classList.add('show');
 
-        document.querySelector('.pop-up-edit').classList.add('show');
-
-        document.querySelector('#title-edit').value = titleNotesEdit[i].textContent;
-        document.querySelector('#content-edit').value = contentNotesEdit[i].textContent;
-
-        document.querySelector('#edit').onclick = function() {
-        document.querySelector('.pop-up-edit').classList.remove('show');
-
-        titleNotesEdit[i].textContent = titleEdit.value;
-        contentNotesEdit[i].textContent = contentEdit.value;
-
-        titleEdit.value = "";
-        contentEdit.value = "";
-
-        }
-      }
-    }
-}
-
-// !date function
-function dateNow() {
-  let date = new Date();
-  let tanggal = date.getDate();
-  let bulanNumber = date.getMonth();
-  let tahun = date.getFullYear();
-  let bulanTeks = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-
-  return `${tanggal} ${bulanTeks[bulanNumber]} ${tahun}`;
-}
-
-// !starred note function
-function starredNotes() {
-  let starField = document.querySelectorAll('.starField');
-  let star = document.querySelectorAll('.starField i');
-
-  for (let i = 0; i < starField.length; i++) {
-    starField[i].onclick = function () {
-      
-      if (this.classList.contains('starred')) {
-        this.innerHTML = '<i class="fa-regular fa-star star"></i>'
-        this.classList.remove('starred')
-      } else {
-        this.innerHTML = '<i class="fa-solid fa-star star starred"></i>';
-        this.classList.add('starred')
-      }
-    }
+  if (notes === null) { // !mengecek apakah ada data atau tidak di local storage
+    noteObj = []; // !jika tidak ada kembalikan array kosong di noteObj
+  } else {
+    noteObj = JSON.parse(notes); // !Jika ada kembalikan datanya dalanya dengan mengubah datanya dari string menjadi object
   }
 
+  addTitle.value = noteObj[index].title;
+  addText.value = noteObj[index].text;
+
+  noteObj.splice(index, 1);
+  localStorage.setItem("NOTES", JSON.stringify(noteObj));
+
+  renderNotes();
+
 }
 
-// !random color function
+
+// !membuat function untuk merandom warna background note
 function randomColor() {
-  let colors = ['FAEAB1', 'F8F988', 'E5B8F4', 'A5F1E9', 'C7BCA1', 'A4BE7B', 'BA94D1', 'CFF5E7', '8D72E1', 'B8E8FC'];
   let randomNumber = Math.floor(Math.random() * 10);
-  let bgColor = colors[randomNumber]
+  let colors = ['FAEAB1', 'D09CFA', 'E6DDC4', '97DECE', 'FFCAC8', 'FCF9BE', 'FFADBC', 'BA94D1', 'B6E2A1', 'CFB997'];
+
+  let bgColor = `#${colors[randomNumber]}`;
 
   return bgColor;
 }
 
+// !membuat function untuk mencetak tanggal sekarang
+function dateNow() {
+  let now = new Date;
+  let date = now.getDate();
+  let monthNumber = now.getMonth();
+  let year = now.getFullYear();
+  let monthText = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+
+  return `${date} ${monthText[monthNumber]} ${year}`;
+}
+
+renderNotes();
